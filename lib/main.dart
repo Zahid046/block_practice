398adbef5bd30ccda8fc58f0e81c6e50ea5e0c53
Zahid046 <zmemon046@gmail.com>
2022-09-08 15:35:50 +0600

    step1

---
+++ lib/main.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+import 'package:bloc/bloc.dart';
+import 'dart:math' as math show Random;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: const MyHomePage(),
+    );
+  }
+}
+
+const names = [
+  'Foo',
+  'Bar',
+  'Baz',
+];
+
+extension RandomElelement<T> on Iterable<T> {
+  T getRandomElement() => elementAt(math.Random().nextInt(length));
+}
+
+class NamesCubit extends Cubit<String?> {
+  NamesCubit() : super(null);
+
+  void pickRandomName() {
+    emit(names.getRandomElement());
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late final NamesCubit cubit;
+  @override
+  void initState() {
+    super.initState();
+    cubit = NamesCubit();
+  }
+
+  @override
+  void dispose() {
+    cubit.close();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: StreamBuilder<String?>(
+        stream: cubit.stream,
+        builder: (context, snapshot) {
+          final button = TextButton(
+            onPressed: () => cubit.pickRandomName(),
+            child: const Text('Pick a random name'),
+          );
+          switch (snapshot.connectionState) {
+            case ConnectionState.none:
+              return button;
+            case ConnectionState.waiting:
+              return button;
+            case ConnectionState.active:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(snapshot.data ?? 'No name picked yet'),
+                    button,
+                  ],
+                ),
+              );
+            case ConnectionState.done:
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(snapshot.data ?? 'No name picked yet'),
+                    button,
+                  ],
+                ),
+              );
+          }
+        },
+      ),
+    );
+  }
+}
